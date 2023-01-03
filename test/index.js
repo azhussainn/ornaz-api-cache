8b7df57e569ab0f1880674df2d471deb1356a6a2
azhuss77 <admin@ornaz.com>
2023-01-03 18:25:29 +0530

    -

--- test/index.js
+++ test/index.js
@@ -1,61 +1,17 @@
-require("../index.js");
-const {
-  getSearchableFilters,
-} = require("../routes/catlog/utils");
+const app = require("../index.js");
+let chai = require('chai');
+let chaiHttp = require('chai-http');
 
+chai.use(chaiHttp);
 
-describe("test search query and applied filters", () => {
-
-  it("case 1", (done) => {
-    const appliedFilters = [ [ 'metal_color=white-gold' ] ]
-    const searchQuery = [ 'women', 'engagement', 'solitare', 'ring' ]
-    const baseCategory = null
-    const { 
-      finalFilters, searchBaseCategory, potentialNamesArr 
-    } = getSearchableFilters({ appliedFilters, searchQuery, baseCategory });
-    done();
-    console.log(finalFilters,       "<====filters")
-    console.log(searchBaseCategory, "<====base category")
-    console.log(potentialNamesArr,  "<=====potential names\n")
-  });
-
-  it("case 2", (done) => {
-    const appliedFilters = [  ]
-    const searchQuery = [ 'men', 'everyday', 'necklace', 'bre' ]
-    const baseCategory = null
-    const { 
-      finalFilters, searchBaseCategory, potentialNamesArr 
-    } = getSearchableFilters({ appliedFilters, searchQuery, baseCategory });
-    done();
-    console.log(finalFilters, "<====filters")
-    console.log(searchBaseCategory, "<====base category")
-    console.log(potentialNamesArr, "<=====potential names\n")
-  });
-
-  it("case 3", (done) => {
-    const appliedFilters = [ [ 'shape=pear' ] ]
-    const searchQuery = [ 'gift', 'girlfriend', 'proposal' ]
-    const baseCategory = null
-    const { 
-      finalFilters, searchBaseCategory, potentialNamesArr 
-    } = getSearchableFilters({ appliedFilters, searchQuery, baseCategory });
-    done();
-    console.log(finalFilters, "<====filters")
-    console.log(searchBaseCategory, "<====base category")
-    console.log(potentialNamesArr, "<=====potential names\n")
-  });
-
-  it("case 4", (done) => {
-    const appliedFilters = [ ]
-    const searchQuery = [ 'aniversary', 'wife', 'ring', 'diamond' ]
-    const baseCategory = null
-    const { 
-      finalFilters, searchBaseCategory, potentialNamesArr 
-    } = getSearchableFilters({ appliedFilters, searchQuery, baseCategory });
-    done();
-    console.log(finalFilters, "<====filters")
-    console.log(searchBaseCategory, "<====base category")
-    console.log(potentialNamesArr, "<=====potential names\n")
+describe("/GET all catlog data", () => {  
+  it('case 1', (done) => {
+    chai.request(app)
+        .get('/jewellery')
+        .end((err, res) => {
+          
+          done();
+        });
   });
 
 });
